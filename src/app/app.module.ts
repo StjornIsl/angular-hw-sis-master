deea035a98fbe8864af2074101723ce5981cd422
Yury <ryv8@tut.by>
2020-07-16 22:19:58 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TeacherComponent } from './teacher/teacher.component';
+import { StudentComponent } from './student/student.component';
+import {FormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {LoginModule} from '../login/login.module';
+import {RouterModule, Routes} from '@angular/router';
+import {UserComponent} from '../login/user/user.component';
+import {HttpClientModule} from '@angular/common/http';
+
+const  appRoutes: Routes = [
+  {
+    path: 'teacher',
+    component: TeacherComponent
+  },
+  {
+    path: 'student',
+    component: StudentComponent
+  },
+  {
+    path: 'login',
+    component: UserComponent
+  }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    TeacherComponent,
+    StudentComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NgbModule,
+    HttpClientModule,
+    LoginModule,
+    RouterModule.forChild(appRoutes)
+  ],
+  exports: [RouterModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
